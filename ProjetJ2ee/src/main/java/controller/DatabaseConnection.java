d68afde72e547e763ce127ecedcbe836d87049d6
CYTech Student <CYTech Student@Student-laptop>
2024-12-28 21:32:29 +0100

    ajout de la classe connexion à la BDD

--- ProjetJ2ee/src/main/java/controller/DatabaseConnection.java
+++ ProjetJ2ee/src/main/java/controller/DatabaseConnection.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
 
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/bdd_J2EE";
+    private static final String USER = "root";
+    private static final String PASSWORD = "cytech0001";
+ 
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
+
