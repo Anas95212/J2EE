981182a880dea38c7e6662ac416a7e36dc3854df
CYTech Student <CYTech Student@Student-laptop.lan>
2024-12-29 21:46:08 +0100

    modif connexion bdd via alwaysDATA

--- ProjetJ2ee/src/main/java/controller/DatabaseConnection.java
+++ ProjetJ2ee/src/main/java/controller/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
  
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://192.168.1.119:3306/bdd_J2EE";
-    private static final String USER = "root";
-    private static final String PASSWORD = "cytech0001";
+    private static final String URL = "mysql-zinee91.alwaysdata.net";
+    private static final String USER = "zinee91_game";
+    private static final String PASSWORD = "J2EE2025";
  
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
